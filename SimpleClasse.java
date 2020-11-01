e3fc4ed52dfac5a4ffc17c704c42642729263bbf
zheng0zhou <809875104@qq.com>
2020-11-01 17:18:17 +0100

    ajout de la classe SimpleClasse.java

---
+++ SimpleClasse.java
@@ -0,0 +1,12 @@
+import java.io.*; 
+public class SimpleClasse {
+	public int somme(int a, int b){
+		return a+b;
+	}
+	public static void main(String[] args){
+		SimpleClasse sc = new SimpleClasse();
+		int a=3;
+		int b=5;
+		System.out.println("La somme de "+a+" et "+b+" est : "+sc.somme(a,b));	
+	}
+}
